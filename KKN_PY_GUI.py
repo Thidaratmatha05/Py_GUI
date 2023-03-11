d16d30122fb0b484077b327e475ba1189bc15f8f
Thidaratmatha05 <76549405+Thidaratmatha05@users.noreply.github.com>
2023-03-12 00:08:43 +0700

    up GUI kkn

---
+++ KKN_PY_GUI.py
@@ -0,0 +1,228 @@
+from tkinter import * ;
+
+root =Tk()
+root.title("My App K GUI")
+
+Label(text="").pack()
+Label(text="โปรแกรม...",font=50).pack()
+
+frame = Frame(root,bd=3, height=100, width=100, relief=GROOVE, cursor="hand1" , highlightthickness=20)
+frame.pack()
+
+#row=0
+Label(frame,text=" ลำดับที่ ",bg = "white",font=40).grid(row=0,column=0)
+Label(frame,text="").grid(row=0,column=1)
+Label(frame,text=" ข้อมูล01 ",bg = "white",font=40).grid(row=0,column=2)
+Label(frame,text="").grid(row=0,column=3)
+Label(frame,text=" ข้อมูล02 ",bg = "white",font=40).grid(row=0,column=4)
+Label(frame,text="").grid(row=0,column=5)
+Label(frame,text=" ข้อมูล03 ",bg = "white",font=40).grid(row=0,column=6)
+Label(frame,text="").grid(row=0,column=7)
+Label(frame,text=" ข้อมูล04 ",bg = "white",font=40).grid(row=0,column=8)
+
+#row=1
+Label(frame,text="01").grid(row=1,column=0)
+Label(frame,text="").grid(row=1,column=1)
+Label(frame,text="01").grid(row=1,column=2)
+Label(frame,text="").grid(row=1,column=3)
+Label(frame,text="02").grid(row=1,column=4)
+Label(frame,text="").grid(row=1,column=5)
+Label(frame,text="03").grid(row=1,column=6)
+Label(frame,text="").grid(row=1,column=7)
+Label(frame,text="04").grid(row=1,column=8)
+
+#row=2
+Label(frame,text="02").grid(row=2,column=0)
+Label(frame,text="").grid(row=2,column=1)
+Label(frame,text="01").grid(row=2,column=2)
+Label(frame,text="").grid(row=2,column=3)
+Label(frame,text="02").grid(row=2,column=4)
+Label(frame,text="").grid(row=2,column=5)
+Label(frame,text="03").grid(row=2,column=6)
+Label(frame,text="").grid(row=2,column=7)
+Label(frame,text="04").grid(row=2,column=8)
+
+#row=3
+Label(frame,text="03").grid(row=3,column=0)
+Label(frame,text="").grid(row=3,column=1)
+Label(frame,text="01").grid(row=3,column=2)
+Label(frame,text="").grid(row=3,column=3)
+Label(frame,text="02").grid(row=3,column=4)
+Label(frame,text="").grid(row=3,column=5)
+Label(frame,text="03").grid(row=3,column=6)
+Label(frame,text="").grid(row=3,column=7)
+Label(frame,text="04").grid(row=3,column=8)
+
+#row=4
+Label(frame,text="04").grid(row=4,column=0)
+Label(frame,text="").grid(row=4,column=1)
+Label(frame,text="01").grid(row=4,column=2)
+Label(frame,text="").grid(row=4,column=3)
+Label(frame,text="02").grid(row=4,column=4)
+Label(frame,text="").grid(row=4,column=5)
+Label(frame,text="03").grid(row=4,column=6)
+Label(frame,text="").grid(row=4,column=7)
+Label(frame,text="04").grid(row=4,column=8)
+
+#row=5
+Label(frame,text="05").grid(row=5,column=0)
+Label(frame,text="").grid(row=5,column=1)
+Label(frame,text="01").grid(row=5,column=2)
+Label(frame,text="").grid(row=5,column=3)
+Label(frame,text="02").grid(row=5,column=4)
+Label(frame,text="").grid(row=5,column=5)
+Label(frame,text="03").grid(row=5,column=6)
+Label(frame,text="").grid(row=5,column=7)
+Label(frame,text="04").grid(row=5,column=8)
+
+#row=6
+Label(frame,text="06").grid(row=6,column=0)
+Label(frame,text="").grid(row=6,column=1)
+Label(frame,text="01").grid(row=6,column=2)
+Label(frame,text="").grid(row=6,column=3)
+Label(frame,text="02").grid(row=6,column=4)
+Label(frame,text="").grid(row=6,column=5)
+Label(frame,text="03").grid(row=6,column=6)
+Label(frame,text="").grid(row=6,column=7)
+Label(frame,text="04").grid(row=6,column=8)
+
+#row=7
+Label(frame,text="07").grid(row=7,column=0)
+Label(frame,text="").grid(row=7,column=1)
+Label(frame,text="01").grid(row=7,column=2)
+Label(frame,text="").grid(row=7,column=3)
+Label(frame,text="02").grid(row=7,column=4)
+Label(frame,text="").grid(row=7,column=5)
+Label(frame,text="03").grid(row=7,column=6)
+Label(frame,text="").grid(row=7,column=7)
+Label(frame,text="04").grid(row=7,column=8)
+
+#row=8
+Label(frame,text="08").grid(row=8,column=0)
+Label(frame,text="").grid(row=8,column=1)
+Label(frame,text="01").grid(row=8,column=2)
+Label(frame,text="").grid(row=8,column=3)
+Label(frame,text="02").grid(row=8,column=4)
+Label(frame,text="").grid(row=8,column=5)
+Label(frame,text="03").grid(row=8,column=6)
+Label(frame,text="").grid(row=8,column=7)
+Label(frame,text="04").grid(row=8,column=8)
+
+#row=9
+Label(frame,text="09").grid(row=9,column=0)
+Label(frame,text="").grid(row=9,column=1)
+Label(frame,text="01").grid(row=9,column=2)
+Label(frame,text="").grid(row=9,column=3)
+Label(frame,text="02").grid(row=9,column=4)
+Label(frame,text="").grid(row=9,column=5)
+Label(frame,text="03").grid(row=9,column=6)
+Label(frame,text="").grid(row=9,column=7)
+Label(frame,text="04").grid(row=9,column=8)
+
+#row=10
+Label(frame,text="10").grid(row=10,column=0)
+Label(frame,text="").grid(row=10,column=1)
+Label(frame,text="01").grid(row=10,column=2)
+Label(frame,text="").grid(row=10,column=3)
+Label(frame,text="02").grid(row=10,column=4)
+Label(frame,text="").grid(row=10,column=5)
+Label(frame,text="03").grid(row=10,column=6)
+Label(frame,text="").grid(row=10,column=7)
+Label(frame,text="04").grid(row=10,column=8)
+
+
+frame2 = Frame(root,bd=3, height=50, width=50, relief=GROOVE, cursor="hand1" , highlightthickness=20)
+frame2.pack()
+
+#row=0
+Label(frame2,text="ลำดับที่").grid(row=0,column=0)
+Label(frame2,text="ข้อมูลที่1").grid(row=0,column=1)
+
+Label(frame2,text="").grid(row=0,column=2)
+Label(frame2,text="ข้อมูลที่2").grid(row=0,column=3)
+
+Label(frame2,text="").grid(row=0,column=4)
+Label(frame2,text="ข้อมูลที่3").grid(row=0,column=5)
+
+Label(frame2,text="").grid(row=0,column=6)
+Label(frame2,text="ข้อมูลที่4").grid(row=0,column=7)
+
+
+#row=1
+txt = StringVar()
+mytxt = Entry(frame2,textvariable=txt)
+mytxt.grid(row=1,column=0)
+#txt01 = IntVar()
+txt01 = StringVar()
+mytxt01 = Entry(frame2,textvariable=txt01)
+mytxt01.grid(row=1,column=1)
+
+Label(frame2,text="").grid(row=1,column=2)
+txt02 = StringVar()
+mytxt02 = Entry(frame2,textvariable=txt02)
+mytxt02.grid(row=1,column=3)
+
+Label(frame2,text="").grid(row=1,column=4)
+txt03 = StringVar()
+mytxt03 = Entry(frame2,textvariable=txt03)
+mytxt03.grid(row=1,column=5)
+
+Label(frame2,text="").grid(row=1,column=6)
+txt04 = StringVar()
+mytxt04 = Entry(frame2,textvariable="",relief=SUNKEN)
+mytxt04.grid(row=1,column=7)
+
+#row=3
+#Label(frame2,text="ผลทำนาย").grid(row=2,column=0)
+
+Label(frame2,text="").grid(row=2,column=2)
+Label(frame2,text="").grid(row=2,column=4)
+Label(frame2,text="").grid(row=2,column=6)
+
+#row=3 , showtxt
+def showText():
+    Message = txt.get()
+    print(Message)
+    la_txex = Label(frame2,text=Message).grid(row=2,column=0)
+
+    Message01 = txt01.get()
+    print(Message01)
+    la_txex01 = Label(frame2,text=Message01).grid(row=2,column=1)
+
+    Message02 = txt02.get()
+    print(Message02)
+    la_txex02 = Label(frame2,text=Message02).grid(row=2,column=3)
+
+    Message03 = txt03.get()
+    print(Message03)
+    la_txex03 = Label(frame2,text=Message03).grid(row=2,column=5)
+
+    Message04 = txt04.get()
+    print(Message04)
+    la_txex04 = Label(frame2,text=Message04).grid(row=2,column=7)
+
+    mytxt.delete(0,END)
+    mytxt01.delete(0,END)
+    mytxt02.delete(0,END)
+    mytxt03.delete(0,END)
+    mytxt04.delete(0,END)
+
+
+def deleteText():
+    mytxt.delete(0,END)
+    mytxt01.delete(0,END)
+    mytxt02.delete(0,END)
+    mytxt03.delete(0,END)
+    mytxt04.delete(0,END)
+
+
+    
+
+#row=4
+buttonShow = Button(frame2,text='ทำนาย', command=showText).grid(row=3,column=5)
+#buttondelete = Button(frame2,text='deleteText', command=deleteText).grid(row=3,column=7)
+
+
+#กำหนดขนาดหน้าต่าง
+root.geometry("960x540")
+root.mainloop()
